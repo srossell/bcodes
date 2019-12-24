fd63550a4ef97d8ec83d312e76ef1e8360e55e56
Sergio Rossell <sergio.rossell@gmail.com>
2019-12-24 16:42:21 +0100

    update tellurim ** -> ^

--- utils/telluriumstrings.py
+++ utils/telluriumstrings.py
@@ -35,40 +35,44 @@ end
 
 # For rates and functions
 def create_func_str(func_dict):
-    func_str = ''
+    func_str = ""
     for key in func_dict:
-        func_str += '{0}:= {1};\n'.format(key, func_dict[key])
+        func_str += "{0}:= {1};\n".format(key, func_dict[key])
+        func_str = func_str.replace("**", "^")
     return func_str
+
 
 # For parameters and init
 def create_assignment_str(assign_dict):
-    assign_str = ''
+    assign_str = ""
     for key in assign_dict:
-        assign_str += '{0} = {1};\n'.format(key, assign_dict[key])
+        assign_str += "{0} = {1};\n".format(key, assign_dict[key])
     return assign_str
 
+
 def create_massbalance_str(mb_dict):
-    mb_str = ''
+    mb_str = ""
     for sp in mb_dict:
         s = "{0}' = ".format(sp)
         for rxn in mb_dict[sp]:
             if mb_dict[sp][rxn] > 0:
-                sign = '+'
+                sign = "+"
             else:
-                sign = '-'
-            s += '{0} {1} * {2}'.format(sign, abs(mb_dict[sp][rxn]), rxn)
-        s += '; \n'
+                sign = "-"
+            s += "{0} {1} * {2}".format(sign, abs(mb_dict[sp][rxn]), rxn)
+        s += "; \n"
         mb_str += s
     return mb_str
 
+
 def create_sp_str(id_sp):
-    sp_str = 'species '
+    sp_str = "species "
     for sp in id_sp:
-        sp_str += '{0}, '.format(sp)
-    #remove last comma
+        sp_str += "{0}, ".format(sp)
+    # remove last comma
     sp_str = sp_str[:-2]
     return sp_str
 
+
 def create_event_str():
     pass
-
